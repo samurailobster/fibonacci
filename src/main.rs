4eb02978e6f44277a409a148c5ff9b0b21110975
samurailobster <jeffreymccharen@gmail.com>
2020-09-09 07:56:02 -0500

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use num_traits::{Zero, One};
 use std::mem::replace;
 use::std::io;
 
+// added num_bigint for bigger numbers
+// without max seems to be 187
+
 
 fn main() {
 
@@ -21,13 +24,16 @@ fn main() {
     println!("Printing {} Fibonacci numbers..", choice);
     print_fibonacci_numbers(choice);
 
-    fn print_fibonacci_numbers(choice : usize) {}
+    fn print_fibonacci_numbers(choice : usize) {
+        let mut counter: usize = 1;
         let mut f0: BigUint = Zero::zero();
         let mut f1: BigUint = One::one();
         for _ in 0..choice {
             let f2 = f0 + &f1;
             // This is a low cost way of swapping f0 with f1 and f1 with f2.
             f0 = replace(&mut f1, f2);
-            println!("{} => {}", _ , f0);
+            println!("Fibonacci number {}: {}",counter, f0);
+            counter += 1;
         }
     }
+}
